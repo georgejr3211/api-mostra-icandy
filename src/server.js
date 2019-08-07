d0848f9576140a5dbfbccc223156fe25a1b481ec
George Alexandre <georgefeitosajr12@gmail.com>
2019-08-07 20:32:55 -0300

    Adicionando o primeiro crud da API

--- src/server.js
+++ src/server.js
@@ -1,17 +1,8 @@
-import Sequelize from 'sequelize';
-import config from './config/database';
-
 import App from './app';
 
 const server = App;
-const sequelize = new Sequelize(config);
-
-sequelize
-  .authenticate()
-  .then(() => console.log('Success'))
-  .catch(() => console.log('Err'));
 
 const PORT = process.env.PORT || 4000;
 const HOST = '0.0.0.0';
 
-server.listen(PORT, HOST, () => console.log('Server ON at PORT', PORT));
+server.listen(PORT, HOST, () => console.log('Server listening on port', PORT));
