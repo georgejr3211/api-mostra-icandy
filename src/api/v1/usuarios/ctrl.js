fc3608c083ed52383a185494356a115f349a0552
danielrsant <danrsant14@gmail.com>
2019-08-18 18:14:16 -0300

    criando validacao de cpf em usuarios

--- src/api/v1/usuarios/ctrl.js
+++ src/api/v1/usuarios/ctrl.js
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import bcrypt from 'bcryptjs';
+import { validateBr } from 'js-brasil';
 import * as resourceService from './service';
+
 
 const router = Router();
 
@@ -34,14 +36,18 @@ router.get('/:id', async (req, res, next) => {
 
 router.post('/', async (req, res, next) => {
   try {
-    req.body.password = bcrypt.hashSync(req.body.password);
-    let resource = await resourceService.createResource(req.body);
-    resource = await resourceService.getResource(resource.id);
-    return res.json({
-      value: resource,
-    });
+    const cpf = validateBr.cpf(req.body.cpf);
+    if (cpf) {
+      req.body.password = bcrypt.hashSync(req.body.password);
+      let resource = await resourceService.createResource(req.body);
+      resource = await resourceService.getResource(resource.id);
+      return res.json({
+        value: resource,
+      });
+    }
+    throw new Error('CPF INVÁLIDO!');
   } catch (error) {
-    return next(error);
+    return next(error.message);
   }
 });
 
