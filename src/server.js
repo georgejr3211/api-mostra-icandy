9773262c052606754f5c4a88ec8e3e4ab5da99a3
George Alexandre <georgefeitosajr12@gmail.com>
2019-10-15 22:08:40 -0300

    comentando a relation

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 import App from './app';
 
-const PORT = process.env.PORT || 3000;
+const server = require('http').createServer(App);
+const io = require('socket.io')(server);
 
-const server = App.listen(PORT, () => console.log('Server listening on port', PORT));
-const io = require('socket.io').listen(server);
+const PORT = process.env.PORT || 3000;
+const HOST = '0.0.0.0';
+
+console.log('deu certo');
 
 io.on('connection', (socket) => {
   socket.on('update-status', async (status) => {
@@ -11,11 +14,4 @@ io.on('connection', (socket) => {
   });
 });
 
-// const server = require('http').createServer(App);
-// const io = require('socket.io')(server);
-
-// const HOST = '0.0.0.0';
-
-// console.log('deu certo');
-
-// server.listen(PORT, HOST, () => console.log('Server listening on port', PORT));
+server.listen(PORT, HOST, () => console.log('Server listening on port', PORT));
